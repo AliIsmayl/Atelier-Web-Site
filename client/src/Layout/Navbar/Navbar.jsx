270a8c252f7c1af7c546c36f7090ad6fda0a4393
AliIsmayl <ali.ismayil.681@gmail.com>
2024-10-16 12:56:09 +0400

    suit image changed

--- client/src/Layout/Navbar/Navbar.jsx
+++ client/src/Layout/Navbar/Navbar.jsx
@@ -1,23 +1,44 @@
-import React from 'react'
-import './Navbar.scss'
-import Logo from '../../Image/atelierr 1.png'
+import React, { useState, useEffect } from 'react';
+import './Navbar.scss';
+import Logo from '../../Image/atelierr 1.png';
+import { IoCloseSharp } from "react-icons/io5";
+import { IoMdMenu } from "react-icons/io";
+
 function Navbar() {
+    const [isRed, setIsRed] = useState(false);
+    const [openNav, setOpenNav] = useState(false)
+
+    function handleOpen() {
+        setOpenNav(!openNav)
+    }
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsRed(window.scrollY > 500);
+        };
+
+        handleScroll();
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
     return (
-        <nav>
-            <img src={Logo} alt="" />
+        <nav style={{ backgroundColor: isRed ? '#47423f' : '' }}>
+            <img src={Logo} alt="Logo" />
             <ul className='normalBox'>
                 <li>About Us</li>
                 <li>Service</li>
                 <li>Contact</li>
             </ul>
-            {/* <ul className='respNavbar'>
+            <div className="btn" style={{ color: isRed ? '#dfd8c9' : '#47423f' }} onClick={handleOpen}>
+                {openNav ? <IoMdMenu /> : <IoCloseSharp />}
+            </div>
+            <ul className={`respNavbar ${openNav ? "opened" : ""}`}>
                 <li>About Us</li>
                 <li>Service</li>
                 <li>Contact</li>
-            </ul> */}
-
+            </ul>
         </nav>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
